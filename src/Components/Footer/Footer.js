7f5388208ac89ad495abec23bc4974f3a96bb566
sibyldawn <sibyldawn@gmail.com>
2018-08-27 10:56:15 -0700

    Add Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+import { withStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import SupervisedUserCircle from '@material-ui/icons/SupervisedUserCircle';
+import MessageSharp from '@material-ui/icons/MessageSharp';
+import SwapHorizSharp from '@material-ui/icons/SwapHorizSharp';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Settings from '@material-ui/icons/Settings';
 
-export default class Footer extends Component {
+
+const styles = {
+  root: {
+    position: 'absolute',
+    left:0,
+    bottom: 0,
+    width: '100vw',
+    background: '#5ACCC1'
+  },
+};
+
+class Footer extends Component {
+  state = {
+    value: 'messages',
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
-    return <div>
-      <h1>THIS IS THE FOOTER!</h1>
-    </div>
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <BottomNavigation value={value} onChange={this.handleChange} className={classes.root}>
+        <BottomNavigationAction label="Messages" value="messages" icon={<MessageSharp />} />
+        <BottomNavigationAction label="Matches" value="matches" icon={<SupervisedUserCircle />} />
+        <BottomNavigationAction label="Finder" value="finder" icon={<SwapHorizSharp />} />
+        <BottomNavigationAction label="Map" value="map" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="Settings" value="settings" icon={<Settings />} />
+      </BottomNavigation>
+    );
   }
 }
+
+Footer.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withRouter(withStyles(styles)(Footer));
+
+
+// import React, { Component } from 'react';
+
+// // export default class Footer extends Component {
+// //   render() {
+// //     return <div>
+// //       <h1>THIS IS THE FOOTER!</h1>
+// //     </div>
+// //   }
+// // }
