e14dfae36b683533b0459cd91739b877d337f7d2
sibyldawn <sibyldawn@gmail.com>
2018-08-27 12:01:40 -0700

    Bottom Navigation Lost Animation

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -84,8 +84,7 @@ class Header extends Component {
                   open={open}
                   onClose={this.handleClose}
                 >
-                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                  <MenuItem onClick={this.handleClose}>Logout</MenuItem>
                 </Menu>
               </div>
             )}
