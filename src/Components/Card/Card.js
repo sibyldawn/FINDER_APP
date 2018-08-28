035b95627e5c37c981afbe7d713cdfa621860d74
olafaloofian <bulluffalo@gmail.com>
2018-08-28 10:10:23 -0700

    Fix card component

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -46,7 +46,7 @@ const styles = theme => ({
   },
 });
 
-class Card extends React.Component {
+class Cards extends React.Component {
   state = { expanded: false };
 
   handleExpandClick = () => {
@@ -139,4 +139,4 @@ Card.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(Card);
+export default withStyles(styles)(Cards);
