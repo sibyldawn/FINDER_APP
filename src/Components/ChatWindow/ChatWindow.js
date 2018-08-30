7956c979e21798ae74d188a4a91b37ee1d38317d
sibyldawn <sibyldawn@gmail.com>
2018-08-30 14:03:10 -0700

    Add ChatKit API

--- src/Components/ChatWindow/ChatWindow.js
+++ src/Components/ChatWindow/ChatWindow.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MessageFeed from './MessageFeed';
 import ChatForm from './ChatForm';
+import Chatkit from '@pusher/chatkit';
 
 
 export default class ChatWindow extends Component {
@@ -8,10 +9,31 @@ export default class ChatWindow extends Component {
         super()
     }
 
+    componentDidMount(){
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator:`${process.env.REACT_APP_INSTANCE_LOCATOR}`,
+            userId: 'Admin',
+            tokenProvider: new Chatkit.TokenProvider({
+                url: 'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/4845df4a-abc6-4f35-87cf-999c9f6d448d/token'
+            })
+        })
+        chatManager.connect()
+        .then(currentUser => {
+            currentUser.subscribeToRoom({
+                 roomId:15069282,
+                 hooks:{
+                     onNewMessage: message => {
+                         console.log('message.text', message.text);
+                     }
+                 }
+            })
+        })
+    }
+
     render() {
         return (
             <div className="chat-window"> CHAT WINDOW
-                <Message/>
+                <MessageFeed/>
                 <ChatForm/>
             
             </div>
--- src/Components/ChatWindow/MessageFeed.js
+++ src/Components/ChatWindow/MessageFeed.js
@@ -1,10 +1,67 @@
 import React, { Component } from 'react';
-import Message from './Message';
+import socketIoClient from 'socket.io-client';
+const socket = socketIoClient("http://localhost:4000");
 
-export default class MessageFeed extends Component {
+// const socketUrl = "http://localhost:4000";
+
+const dummy_text = [
+    {
+        senderId: '1',
+        text:'Hey, what are you doing?'
+    },
+    {
+        senderId: '2',
+        text:'Just chilling,how about you?'
+    },
+    {
+        senderId:'1',
+        text: 'Want to go out for lunch?'
+    },
+    {
+        senderId:'2',
+        text: 'Sure!'
+    }
+       
+
+]
+
+
+export default class Message extends Component {
+    constructor(props){
+        super(props);
+
+        this.state={
+        }
+
+        socket.on("message", (message)=> {
+            this.setState({
+                message: message
+            })
+        })
+
+    }
+
+    componentDidMount(){
+    }
+
+    message =()=> {
+        socket.emit("sibyl", "hello")
+    }
+    
     render() {
         return (
-            <div>Message Feed</div>
+            <div className='message-list'>
+                {dummy_text.map((message,index) => {
+                    return(
+                        <div className='message' key={index}>
+                            <span className='message-sender'>{message.senderId}</span>
+                            <span className='message-text'>{message.text}</span>
+                        </div>
+                    )
+                })}
+                {this.state.message}
+                <button onClick={this.message}>press</button>
+            </div>
         );
     }
 }
