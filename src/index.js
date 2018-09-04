3e1cc9d3b95d4343e990f577b1739dab5bc5d2b0
olafaloofian <bulluffalo@gmail.com>
2018-09-04 11:23:58 -0700

    Add spicy hot reloading on front end

--- src/index.js
+++ src/index.js
@@ -4,22 +4,27 @@ import './index.css';
 import App from './App';
 import ContextProvider from './ContextAPI/ContextProvider'
 import { BrowserRouter as Router } from 'react-router-dom'
-// import { AppContainer } from 'react-hot-loader';
+// For hot reloading
+import { AppContainer } from 'react-hot-loader';
 
-// if (module.hot) {
-//     module.hot.accept();
-//   }
+const render = () => { 
+    ReactDOM.render(
+        <AppContainer>
+            <Router>
+                <ContextProvider>
+                    <App /> 
+                </ ContextProvider>
+            </ Router>
+        </ AppContainer>
+        , document.getElementById('root')
+    );
+}
 
+// Hot reloading below
+render()
 
-// const render = () => { 
-ReactDOM.render(
-
-    <Router>
-        <ContextProvider>
-            <App /> 
-        </ ContextProvider>
-    </ Router>
-    , document.getElementById('root')
-    
-);
-
+if (module.hot) {
+    module.hot.accept('./App', () => {
+        render()
+    })
+}
