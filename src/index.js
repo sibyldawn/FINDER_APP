a257f9c7d020a65519f7ddb7ed47a7ac2bc5b99d
Eric Rivera <ericrivera@Eric-Riveras-MacBook-Pro.local>
2018-09-04 11:07:33 -0700

    new changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import './index.css';
 import App from './App';
 import ContextProvider from './ContextAPI/ContextProvider'
 import { BrowserRouter as Router } from 'react-router-dom'
+// import { AppContainer } from 'react-hot-loader';
 
+// if (module.hot) {
+//     module.hot.accept();
+//   }
+
+
+// const render = () => { 
 ReactDOM.render(
 
     <Router>
