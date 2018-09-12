1d634908bbe833ad4f1e30a58716a48f662941b6
sibyldawn <sibyldawn@gmail.com>
2018-09-12 00:18:01 -0700

    Add tentative Cypress Test

--- cypress/integration/landing_spec.js
+++ cypress/integration/landing_spec.js
@@ -36,4 +36,22 @@ describe('Landing Page Test', function() {
         cy.get('.no-user-landing').find('.howTo-button').click({force:true})
         cy.window()
  })
+
+ it('Login Button should start Auth0', function() {
+    cy.visit('/')
+    cy.on('uncaught:exception', (err, runnable) => {
+        expect(err.message).to.include('<')
+    
+        // using mocha's async done callback to finish
+        // this test so we prove that an uncaught exception
+        // was thrown
+        done()
+    
+        // return false to prevent the error from
+        // failing this test
+        return false
+    })
+    cy.get('.no-user-landing').find('.login-button').click({force:true})
+})
+
 }) 
