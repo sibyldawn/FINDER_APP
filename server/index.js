510e1187e045da80f2f08959255cb4a1933aba77
sibyldawn <sibyldawn@gmail.com>
2018-09-20 15:55:22 -0700

    Redirect Changed to HTTPS

--- server/index.js
+++ server/index.js
@@ -157,7 +157,7 @@ app.get('/auth/callback', (req, res) => {
         client_secret: process.env.AUTH0_CLIENT_SECRET,
         code: req.query.code,
         grant_type: 'authorization_code',
-        redirect_uri: `http://${req.headers.host}/auth/callback`
+        redirect_uri: `https://${req.headers.host}/auth/callback`
     }
     // Trading a token for an access code at Auth0
     function tradeCodeForAccessToken() {
